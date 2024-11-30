eee2296bf5d754f05563ed67e981c044256b6fed
Muaz Zakaria <muazzakaria@MacBook-Air-von-MacSzerezcom.local>
2024-11-30 17:08:24 +0100

    added header to response

--- app/main.go
+++ app/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
 
-// Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
 var _ = net.ListenUDP
 
 func main() {
@@ -20,21 +20,58 @@ func main() {
 		fmt.Println("Failed to bind to address:", err)
 		return
 	}
+
 	defer udpConn.Close()
+
 	buf := make([]byte, 512)
+
 	for {
 		size, source, err := udpConn.ReadFromUDP(buf)
 		if err != nil {
 			fmt.Println("Error receiving data:", err)
 			break
 		}
+
 		receivedData := string(buf[:size])
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-		// Create an empty response
-		response := []byte{}
-		_, err = udpConn.WriteToUDP(response, source)
+
+		// Construct the DNS header
+		// Fields:
+		// ID = 1234 (0x04D2),
+		// QR = 1,
+		// OPCODE = 0,
+		// AA = 0,
+		// TC = 0,
+		// RD = 0
+		// RA = 0,
+		// Z = 0,
+		// RCODE = 0,
+		// QDCOUNT = 0,
+		// ANCOUNT = 0,
+		// NSCOUNT = 0,
+		// ARCOUNT = 0
+
+		headers := [12]byte{}
+
+		// 2 bytes of id
+		binary.BigEndian.PutUint16(headers[:2], 1234) // ID = 1234 (0x04D2),
+
+		// 1 byte of QR, OPCODE, AA, TC, RD AND
+		headers[2] = (1 << 7) | // qr msb
+			(0 << 3) | // opcode 3 msb
+			(0 << 2) | // aa // 2 msb
+			(0 << 2) | // tc
+			(0 << 1) // rd
+
+		// rest is 0
+		headers[3] = 0
+
+		_, err = udpConn.WriteToUDP(headers[:], source)
+
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
+
 	}
+
 }
