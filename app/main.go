35fa847b3785fc70bc184794f9dcfe48dbe5a8e1
Muaz Zakaria <muazzakaria@MacBook-Air-von-MacSzerezcom.local>
2024-11-30 18:59:16 +0100

    - write question section

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
+	"strings"
 )
 
 var _ = net.ListenUDP
@@ -37,24 +38,31 @@ func main() {
 
 		// Construct the DNS header
 		// Fields:
-		// ID = 1234 (0x04D2),
-		// QR = 1,
-		// OPCODE = 0,
-		// AA = 0,
-		// TC = 0,
-		// RD = 0
-		// RA = 0,
-		// Z = 0,
-		// RCODE = 0,
-		// QDCOUNT = 0,
-		// ANCOUNT = 0,
-		// NSCOUNT = 0,
-		// ARCOUNT = 0
+		// ============= 2 bytes ================
+		// ID = 1234, 16 bits
+		// ============= 1 byte ================
+		// QR = 1, 1 bit
+		// OPCODE = 0, 4 bits
+		// AA = 0, 1 bit
+		// TC = 0, 1 bit
+		// RD = 0, 1 bit
+		// ============= 1 byte ================
+		// RA = 0, 1 bit
+		// Z = 0, 3 bits
+		// RCODE = 0, 4 bits
+		// ============= 2 bytes ================
+		// QDCOUNT = 0, 16 bits
+		// ============= 2 bytes ================
+		// ANCOUNT = 0, 16 bits
+		// ============= 2 bytes ================
+		// NSCOUNT = 0, 16 bits
+		// ============= 2 bytes ================
+		// ARCOUNT = 0 16 bits
 
 		headers := [12]byte{}
 
 		// 2 bytes of id
-		binary.BigEndian.PutUint16(headers[:2], 1234) // ID = 1234 (0x04D2),
+		binary.BigEndian.PutUint16(headers[:2], binary.BigEndian.Uint16(buf[:2])) // ID = 1234 (0x04D2),
 
 		// 1 byte of QR, OPCODE, AA, TC, RD AND
 		headers[2] = (1 << 7) | // qr msb
@@ -63,15 +71,50 @@ func main() {
 			(0 << 2) | // tc
 			(0 << 1) // rd
 
-		// rest is 0
 		headers[3] = 0
 
-		_, err = udpConn.WriteToUDP(headers[:], source)
+		binary.BigEndian.PutUint16(headers[4:6], uint16(1))
+		binary.BigEndian.PutUint16(headers[6:8], 0)
+		binary.BigEndian.PutUint16(headers[8:10], 0)
+		binary.BigEndian.PutUint16(headers[10:12], 0)
+
+		questions := []byte{}
+
+		// questions section
+		questions = append(questions, ParseString("codecrafters.io")...)
+		questions = append(questions, ParseBigEndianUint16(1)...) // type
+		questions = append(questions, ParseBigEndianUint16(1)...) // class
+
+		fullContent := append(headers[:], questions...)
+
+		_, err = udpConn.WriteToUDP(fullContent, source)
 
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
 
 	}
+}
 
+func ParseBigEndianUint16(num uint16) []byte {
+	bytes := make([]byte, 2)
+	binary.BigEndian.PutUint16(bytes, uint16(num))
+
+	return []byte(bytes)
+}
+
+func ParseString(domainName string) []byte {
+	// split at .
+	parts := strings.Split(domainName, ".")
+
+	result := []byte{}
+
+	for _, part := range parts {
+		result = append(result, byte(len(part)))
+		result = append(result, []byte(part)...)
+	}
+
+	result = append(result, '\000')
+
+	return result
 }
