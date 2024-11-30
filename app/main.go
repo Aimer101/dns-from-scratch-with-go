8641c8dc8ce8f89b66d13daf309ddec74925c5b5
Muaz Zakaria <muazzakaria@MacBook-Air-von-MacSzerezcom.local>
2024-11-30 23:10:42 +0100

    - parse client's headers

--- app/main.go
+++ app/main.go
@@ -1,13 +1,126 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"net"
 	"strings"
 )
 
-var _ = net.ListenUDP
+// Construct the DNS header
+// Fields:
+// ============= 2 bytes ================
+// ID = 1234, 16 bits
+// ============= 1 byte ================
+// QR = 1, 1 bit
+// OPCODE = 0, 4 bits
+// AA = 0, 1 bit
+// TC = 0, 1 bit
+// RD = 0, 1 bit
+// ============= 1 byte ================
+// RA = 0, 1 bit
+// Z = 0, 3 bits
+// RCODE = 0, 4 bits
+// ============= 2 bytes ================
+// QDCOUNT = 16 bits
+// ============= 2 bytes ================
+// ANCOUNT = 16 bits
+// ============= 2 bytes ================
+// NSCOUNT = 16 bits
+// ============= 2 bytes ================
+// ARCOUNT = 16 bits
+//
+//	type DnsHeaderDTO struct {
+//		ID      uint16
+//		QR      uint8 // 1 bit
+//		OP      uint8 // 4 bits
+//		AA      uint8 // 1 bit
+//		TC      uint8 // 1 bit
+//		RD      uint8 // 1 bit
+//		RA      uint8 // 1 bit
+//		Z       uint8 // 3 bits
+//		RCODE   uint8 // 4 bits
+//		QDCOUNT uint16
+//		ANCOUNT uint16
+//		NSCOUNT uint16
+//		ARCOUNT uint16
+//	}
+type DnsHeaderDTO struct {
+	ID             uint16
+	QR_OP_AA_TC_RD uint8
+	RA_Z_RCODE     uint8
+	QDCOUNT        uint16
+	ANCOUNT        uint16
+	NSCOUNT        uint16
+	ARCOUNT        uint16
+}
+
+type DnsHeader struct {
+	ID      uint16
+	QR      uint8 // 1 bit
+	OP_CODE uint8 // 4 bits
+	AA      uint8 // 1 bit
+	TC      uint8 // 1 bit
+	RD      uint8 // 1 bit
+	RA      uint8 // 1 bit
+	Z       uint8 // 3 bits
+	RCODE   uint8 // 4 bits
+	QDCOUNT uint16
+	ANCOUNT uint16
+	NSCOUNT uint16
+	ARCOUNT uint16
+}
+
+func (d *DnsHeader) EncodeHeaderDTO() []byte {
+	// The first byte will hold QR, OP_CODE, AA, TC, RD, RA, Z, and RCODE
+	var flags uint16
+
+	// Packing the bits from struct fields into the flags byte.
+	// QR (1 bit)
+	flags |= uint16(d.QR) << 15 // QR is the highest bit (bit 15)
+
+	// OPCODE (4 bits)
+	flags |= uint16(d.OP_CODE) << 11 // OPCODE is next (bits 11-14)
+
+	// AA (1 bit)
+	flags |= uint16(d.AA) << 10 // AA is at bit 10
+
+	// TC (1 bit)
+	flags |= uint16(d.TC) << 9 // TC is at bit 9
+
+	// RD (1 bit)
+	flags |= uint16(d.RD) << 8 // RD is at bit 8
+
+	// RA (1 bit)
+	flags |= uint16(d.RA) << 7 // RA is at bit 7
+
+	// Z (3 bits)
+	flags |= uint16(d.Z) << 4 // Z is at bits 4-6
+
+	// RCODE (4 bits)
+	flags |= uint16(d.RCODE) // RCODE is at bits 0-3
+
+	payload := DnsHeaderDTO{
+		ID:             d.ID,
+		QR_OP_AA_TC_RD: uint8(flags >> 8),
+		RA_Z_RCODE:     uint8(flags & 0xFF),
+		QDCOUNT:        d.QDCOUNT,
+		ANCOUNT:        d.ANCOUNT,
+		NSCOUNT:        d.NSCOUNT,
+		ARCOUNT:        d.ARCOUNT,
+	}
+
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, payload)
+
+	if err != nil {
+		fmt.Println("Failed to encode DNS header:", err)
+	}
+
+	// Return the packed flag byte
+	return buf.Bytes()
+}
 
 func main() {
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
@@ -33,50 +146,52 @@ func main() {
 			break
 		}
 
-		receivedData := string(buf[:size])
-		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
+		requestHeaders := ToDTO(buf[:size])
 
-		// Construct the DNS header
-		// Fields:
-		// ============= 2 bytes ================
-		// ID = 1234, 16 bits
-		// ============= 1 byte ================
-		// QR = 1, 1 bit
-		// OPCODE = 0, 4 bits
-		// AA = 0, 1 bit
-		// TC = 0, 1 bit
-		// RD = 0, 1 bit
-		// ============= 1 byte ================
-		// RA = 0, 1 bit
-		// Z = 0, 3 bits
-		// RCODE = 0, 4 bits
-		// ============= 2 bytes ================
-		// QDCOUNT = 16 bits
-		// ============= 2 bytes ================
-		// ANCOUNT = 16 bits
-		// ============= 2 bytes ================
-		// NSCOUNT = 16 bits
-		// ============= 2 bytes ================
-		// ARCOUNT = 16 bits
+		responseHeader := DnsHeader{
+			ID:      requestHeaders.ID,
+			QR:      1,
+			OP_CODE: requestHeaders.OP_CODE,
+			AA:      0,
+			TC:      0,
+			RD:      requestHeaders.RD,
+			RA:      0,
+			Z:       0,
+			RCODE:   4,
+			QDCOUNT: 1,
+			ANCOUNT: 1,
+			NSCOUNT: 0,
+			ARCOUNT: 0,
+		}
 
-		headers := [12]byte{}
+		// headers := []byte{}
 
-		// 2 bytes of id
-		binary.BigEndian.PutUint16(headers[:2], binary.BigEndian.Uint16(buf[:2])) // ID = 1234 (0x04D2),
+		// // 2 bytes of id
+		// headers = append(headers, ParseBigEndianUint(uint16(responseHeader.ID))...)
 
-		// 1 byte of QR, OPCODE, AA, TC, RD AND
-		headers[2] = (1 << 7) | // qr msb
-			(0 << 3) | // opcode 3 msb
-			(0 << 2) | // aa // 2 msb
-			(0 << 2) | // tc
-			(0 << 1) // rd
+		// // 2 bytes for flags
+		// headers = append(headers, byte(responseHeader.EncodeHeaderDTO()))
 
-		headers[3] = 0
+		headers := responseHeader.EncodeHeaderDTO()
 
-		binary.BigEndian.PutUint16(headers[4:6], uint16(1)) // QDCOUNT
-		binary.BigEndian.PutUint16(headers[6:8], uint16(1)) //ANCOUNT
-		binary.BigEndian.PutUint16(headers[8:10], 0)
-		binary.BigEndian.PutUint16(headers[10:12], 0)
+		// headers := [12]byte{}
+
+		// // 2 bytes of id
+		// binary.BigEndian.PutUint16(headers[:2], binary.BigEndian.Uint16(buf[:2])) // ID = 1234 (0x04D2),
+
+		// // 1 byte of QR, OPCODE, AA, TC, RD AND
+		// headers[2] = (1 << 7) | // qr msb
+		// 	(0 << 3) | // opcode 3 msb
+		// 	(0 << 2) | // aa // 2 msb
+		// 	(0 << 2) | // tc
+		// 	(0 << 1) // rd
+
+		// headers[3] = 0
+
+		// binary.BigEndian.PutUint16(headers[4:6], uint16(1)) // QDCOUNT
+		// binary.BigEndian.PutUint16(headers[6:8], uint16(1)) //ANCOUNT
+		// binary.BigEndian.PutUint16(headers[8:10], 0)
+		// binary.BigEndian.PutUint16(headers[10:12], 0)
 
 		questions := []byte{}
 
@@ -106,6 +221,37 @@ func main() {
 		}
 
 	}
+}
+
+func ToDTO(request []byte) DnsHeader {
+	if len(request) < 12 {
+		panic("incorrect packet size")
+	}
+
+	var parsedRequest DnsHeaderDTO
+
+	err := binary.Read(bytes.NewReader(request), binary.BigEndian, &parsedRequest)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return DnsHeader{
+		ID:      parsedRequest.ID,
+		QR:      parsedRequest.QR_OP_AA_TC_RD >> 7 & 1,
+		OP_CODE: parsedRequest.QR_OP_AA_TC_RD >> 3 & 15,
+		AA:      parsedRequest.QR_OP_AA_TC_RD >> 2 & 1,
+		TC:      parsedRequest.QR_OP_AA_TC_RD >> 1 & 1,
+		RD:      parsedRequest.QR_OP_AA_TC_RD & 1,
+		RA:      parsedRequest.RA_Z_RCODE >> 7 & 1,
+		Z:       parsedRequest.RA_Z_RCODE >> 3 & 7,
+		RCODE:   parsedRequest.RA_Z_RCODE & 15,
+		QDCOUNT: parsedRequest.QDCOUNT,
+		ANCOUNT: parsedRequest.ANCOUNT,
+		NSCOUNT: parsedRequest.NSCOUNT,
+		ARCOUNT: parsedRequest.ARCOUNT,
+	}
+
 }
 
 func ParseBigEndianUint[T uint16 | uint32](num T) []byte {
